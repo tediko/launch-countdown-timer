e334463a88bfdedb6fcd074a5dd96df5bfd581d0
tediko <tediko123@wp.pl>
2021-05-02 16:41:12 +0200

    Added initial website state for FM screenshoot

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,6 @@
 import Timer from './timer.js';
 
-let days = new Timer(777600, 'data-timer-card="days"');
+/* let days = new Timer(777600, 'data-timer-card="days"');
 let hours = new Timer(777600, 'data-timer-card="hours"');
 let minutes = new Timer(777600, 'data-timer-card="minutes"');
-let seconds = new Timer(777600, 'data-timer-card="seconds"');
+let seconds = new Timer(777600, 'data-timer-card="seconds"'); */
